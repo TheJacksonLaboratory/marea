5e2e88e3ee87d8977e1aa6f9403bacbc7aaaf523
Hannah Blau <hannah.blau@jax.org>
2020-04-05 22:08:17 -0400

    SPARQL CONSTRUCT query to get descendants in MeSH for specified descriptor, prints them ordered by label.

--- scripts/query_mesh.py
+++ scripts/query_mesh.py
@@ -1,26 +1,32 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import SPARQLWrapper, RDFXML
+from rdflib import Graph
 
 sparql = SPARQLWrapper("http://id.nlm.nih.gov/mesh/sparql")
 sparql.setQuery("""
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX meshv: <http://id.nlm.nih.gov/mesh/vocab#>
     PREFIX mesh: <http://id.nlm.nih.gov/mesh/>
-    
-    SELECT DISTINCT ?descriptor ?label
-    FROM <http://id.nlm.nih.gov/mesh>
 
+    CONSTRUCT {
+    ?descriptor rdfs:label ?label
+    }
     WHERE {
-    mesh:D009369 meshv:treeNumber ?treeNum .
+    mesh:D013568 meshv:treeNumber ?treeNum .
     ?childTreeNum meshv:parentTreeNumber+ ?treeNum .
     ?descriptor meshv:treeNumber ?childTreeNum .
     ?descriptor rdfs:label ?label .
     }
-
-    ORDER BY ?label
 """)
 
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-# print(results.serialize(format='xml'))
-for result in results["results"]["bindings"]:
-    print("{}\t{}".format(result["descriptor"]["value"], result["label"]["value"]))
+sparql.setReturnFormat(RDFXML)
+results = sparql.queryAndConvert()
+g = Graph()
+g.parse(data=results, format='xml')
+print(len(g))
+
+descriptors = {}
+for s, p, o in g:
+    descriptors[o.toPython()] = s.toPython()
+
+# Print the descriptors sorted by their labels
+[ print("{}\t{}".format(label, descriptor)) for (descriptor, label) in sorted(descriptors.items()) ]

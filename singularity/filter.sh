2124fd9c404a8dcad0403eab771b9db2934b8c6c
Hannah Blau <hannah.blau@jax.org>
2021-01-27 23:32:33 -0500

    Deleted old download code. Separated the singularity slurm scripts so that each runs only one Python program, not two.

--- singularity/filter.sh
+++ singularity/filter.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --qos=batch
-#SBATCH --time=48:00:00
+#SBATCH --time=23:00:00
 #SBATCH --mem-per-cpu=10G
 #SBATCH --mail-user=hannah.blau@jax.org
 #SBATCH --mail-type=END,FAIL
 
 module load singularity
 
-singularity exec marea_python.sif python ../scripts/xml2txt.py -x /projects/robinson-lab/marea/data/pubmed_xml \
- -t /projects/robinson-lab/marea/data/pubmed_txt
 singularity exec marea_python.sif python ../scripts/filter_abstracts.py -i /projects/robinson-lab/marea/data/pubmed_txt \
  -n /projects/robinson-lab/marea/data/nltk_data -o /projects/robinson-lab/marea/data/pubmed_rel D009369 D011494
--- singularity/pubtate.sh
+++ singularity/pubtate.sh
@@ -2,11 +2,10 @@
 #SBATCH --ntasks=1
 #SBATCH --qos=batch
 #SBATCH --time=23:00:00
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=10G
 #SBATCH --mail-user=hannah.blau@jax.org
 #SBATCH --mail-type=END,FAIL
 
 module load singularity
 
-singularity exec marea_python.sif python ../scripts/retrieve_pubmed_files.py \
- -x /projects/robinson-lab/marea/data/pubmed_xml/
+singularity exec marea_python.sif python ../scripts/pubtate.py -i /projects/robinson-lab/marea/pubtator/current
